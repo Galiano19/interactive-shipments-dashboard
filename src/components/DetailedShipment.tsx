3f024732357ec20ca565b34f36e69be56d0bf5d7
Carlos Galiano <galihorizon21@gmail.com>
2025-12-11 20:59:28 +0100

    add mutation on saving

--- src/components/DetailedShipment.tsx
+++ src/components/DetailedShipment.tsx
@@ -3,6 +3,7 @@ import Dialog from "./ui/dialog/dialog";
 import "./DetailedShipment.css";
 import { useState } from "react";
 import { shipmentStatus } from "../constants/shipmentStatus";
+import { useUpdateShipment } from "../hooks/useUpdateShipment";
 
 interface DetailedShipmentProps {
   shipment: Shipment;
@@ -13,6 +14,7 @@ export default function DetailedShipment({
   shipment,
   onClose,
 }: DetailedShipmentProps) {
+  const updateShipmentMutation = useUpdateShipment();
   const [formValues, setFormValues] = useState({
     origin: shipment.origin,
     destination: shipment.destination,
@@ -37,14 +39,26 @@ export default function DetailedShipment({
   }
 
   function handleSave() {
-    console.log("Updated shipment:", { formValues });
+    const updatedShipment: Shipment = {
+      ...shipment,
+      ...formValues,
+    };
 
-    onClose();
+    updateShipmentMutation.mutate(updatedShipment, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
   }
 
   return (
     <Dialog title={shipment.id} defaultOpen onClose={onClose}>
       <Dialog.Body>
+        {updateShipmentMutation.isError && (
+          <div className="error_message">
+            Something went wrong, please try again
+          </div>
+        )}
         <form className="edit_form">
           <div className="edit_form_row">
             <label htmlFor="origin">Origin:</label>
@@ -53,6 +67,7 @@ export default function DetailedShipment({
               id="origin"
               name="origin"
               value={formValues.origin}
+              disabled
             />
           </div>
           <div className="edit_form_row">
@@ -63,11 +78,17 @@ export default function DetailedShipment({
               name="destination"
               value={formValues.destination}
               onChange={handleDestinationChange}
+              disabled={updateShipmentMutation.isPending}
             />
           </div>
           <div className="edit_form_row">
             <label htmlFor="status">Status:</label>
-            <select id="status" name="status" onChange={handleStatusChange}>
+            <select
+              id="status"
+              name="status"
+              onChange={handleStatusChange}
+              disabled={updateShipmentMutation.isPending}
+            >
               {shipmentStatus.map((status) => (
                 <option key={status} value={status}>
                   {status}
@@ -82,13 +103,21 @@ export default function DetailedShipment({
               id="estimatedArrival"
               name="estimatedArrival"
               value={formValues.estimatedArrival}
+              disabled
             />
           </div>
         </form>
       </Dialog.Body>
       <Dialog.Footer>
-        <button onClick={onClose}>Close</button>
-        <button onClick={handleSave}>Save</button>
+        <button onClick={onClose} disabled={updateShipmentMutation.isPending}>
+          Close
+        </button>
+        <button
+          onClick={handleSave}
+          disabled={updateShipmentMutation.isPending}
+        >
+          Save
+        </button>
       </Dialog.Footer>
     </Dialog>
   );
